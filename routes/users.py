c4ad1be6fbdf3a9424a7428e698a99f730eb20f6
motta-lucas <lmotta.ds@gmail.com>
2025-10-19 15:39:02 -0300

    user signup and signin endpoints added

--- routes/users.py
+++ routes/users.py
@@ -16,3 +16,19 @@ async def sign_new_user(data: User) -> dict:
     return{
         "message":"User successfully registered"
     }
+
+@user_router.post("/signin")
+async def sign_user_in(user: UserSignIn) -> dict:
+    if users[user.email] not in users:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User does not exist"
+        )
+    if users[user.email].password != user.password:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            details="Wrong credentials passed"
+        )
+    return {
+        "message": "User signed in successfully"
+    }
