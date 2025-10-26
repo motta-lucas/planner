94ffe6d63da141713f97d540d9296b95e117148b
motta-lucas <lmotta.ds@gmail.com>
2025-10-26 14:38:18 -0300

    [Feature] Incorporate a SQL database and implement CRUD operations

--- models/events.py
+++ models/events.py
@@ -23,11 +23,11 @@ class Event(SQLModel, table=True):
         }
 
 class EventUpdate(SQLModel):
-    title: Optional[str]
-    image: Optional[str]
-    description: Optional[str]
-    tags: Optional[List[str]]
-    location: Optional[str]
+    title: Optional[str] = None
+    image: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
+    location: Optional[str] = None
 
     class Config:
         arbitrary_types_allowed = True
