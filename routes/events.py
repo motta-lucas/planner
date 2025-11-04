d83cadfa9609a3943a873c9c99b2d58427c138dd
motta-lucas <lmotta.ds@gmail.com>
2025-11-03 22:58:17 -0300

    All tests adjusted and passing

--- routes/events.py
+++ routes/events.py
@@ -9,72 +9,70 @@ from auth.authenticate import authenticate
 
 event_database = Database(Event)
 
-event_router = APIRouter(
-    tags=["Events"]
-)
+event_router = APIRouter(tags=["Events"])
 
 events = []
 
-@event_router.get("/", response_model = List[Event])
+
+@event_router.get("/", response_model=List[Event])
 async def retrieve_all_events() -> List[Event]:
     events = await event_database.get_all()
     return events
+
 
 @event_router.get("/{id}", response_model=Event)
 async def retrieve_event(id: PydanticObjectId) -> Event:
     event = await event_database.get(id)
     if not event:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Event with supplied ID does not exist"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Event with supplied ID does not exist"
         )
     return event
+
 
 @event_router.post("/new")
 async def create_event(body: Event, user: str = Depends(authenticate)) -> dict:
     body.creator = user
     await event_database.save(body)
-    return{
-        "message": "Event created successfully"
-    }
+    return {"message": "Event created successfully"}
+
 
 @event_router.put("/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, body: EventUpdate, user: str = Depends(authenticate)) -> Event:
     event = await event_database.get(id)
     if event.creator != user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail = "Operation not allowed"
-        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Operation not allowed")
     updated_event = await event_database.update(id, body)
     if not update_event:
         raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = "Event with supplied ID does not exist"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Event with supplied ID does not exist"
         )
     return updated_event
 
 
 @event_router.delete("/{id}")
 async def delete_event(id: PydanticObjectId, user: str = Depends(authenticate)) -> dict:
-    event = await event_database.delete(id)
-    if not event:
+    event_instance = await event_database.get(id)
+    if not event_instance:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Event with supplied ID does not exist"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Event with supplied ID does not exist"
         )
-    if event.creator != user:
+    if event_instance.creator != user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Event not found"
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Opertion not allowed: You are not the event creator",
         )
-    return{
-        "message": "Event deleted successfully"
-    }
+
+    success = await event_database.delete(id)
+    if not success:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to delete event after authorization check",
+        )
+    return {"message": "Event deleted successfully"}
+
 
 @event_router.delete("/")
 async def delete_all_events() -> dict:
     events.clear()
-    return{
-        "message": "Events deleted successfully"
-    }
+    return {"message": "Events deleted successfully"}
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,7 +2,7 @@ import httpx
 import pytest
 
 from auth.jwt_handler import create_access_token
-from models.events import Event
+from models.events import Event, EventUpdate
 
 from typing import AsyncGenerator
 
@@ -15,12 +15,12 @@ async def access_token() -> str:
 @pytest.fixture(scope="session")
 async def mock_event() -> AsyncGenerator[Event, None]:
     new_event = Event(
-        creator="testuser@packt.com",
+        creator="testuser4@packt.com",
         title="FastAPI Book Launch",
         image="https://linktomyimage.com/image.png",
         description="We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
         tags=["python", "fastapi", "book", "launch"],
-        location="Google Meet",
+        location="Google Test",
     )
 
     await Event.insert_one(new_event)
@@ -44,3 +44,72 @@ async def test_get_event(default_client: httpx.AsyncClient, mock_event: Event) -
     assert response.status_code == 200
     assert response.json()["creator"] == mock_event.creator
     assert response.json()["_id"] == str(mock_event.id)
+
+
+@pytest.mark.asyncio
+async def test_post_event(default_client: httpx.AsyncClient, access_token: str) -> None:
+    payload = {
+        "title": "FastAPI Book",
+        "image": "https://linktomyimage.com/image.png",
+        "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+        "tags": ["python", "fastapi", "book", "launch"],
+        "location": "Virtual",
+    }
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"}
+
+    test_response = {"message": "Event created successfully"}
+
+    response = await default_client.post("/event/new", json=payload, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+@pytest.mark.asyncio
+async def test_get_events_count(default_client: httpx.AsyncClient) -> None:
+    response = await default_client.get("/event/")
+
+    events = response.json()
+
+    assert response.status_code == 200
+    assert len(events) == 2
+
+
+@pytest.mark.asyncio
+async def test_update_event(default_client: httpx.AsyncClient, mock_event: Event, access_token: str) -> None:
+    test_payload = {"title": "Updated FastAPI event"}
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+
+    url = f"/event/{str(mock_event.id)}"
+
+    response = await default_client.put(url, json=test_payload, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json()["title"] == test_payload["title"]
+
+
+@pytest.mark.asyncio
+async def test_delete_event(default_client: httpx.AsyncClient, mock_event: Event, access_token: str) -> None:
+    test_response = {"message": "Event deleted successfully"}
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"}
+
+    url = f"/event/{mock_event.id}"
+
+    response = await default_client.delete(url, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
+
+
+@pytest.mark.asyncio
+async def test_get_event_again(default_client: httpx.AsyncClient, mock_event: Event) -> None:
+    url = f"/event/{str(mock_event.id)}"
+    response = await default_client.get(url)
+
+    assert response.status_code == 404
